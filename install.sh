177f6ec3e8b6225d408b3621220fe73dc77dbd91
Kiyoon Kim <yoonkr33@gmail.com>
2022-11-30 17:13:08 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,32 +1,6 @@
 #!/bin/bash
 # Install Neovim locally without root access
 
-add_env_to_profile() {
-	file="$1"
-	envname="$2"
-	envval="$3"
-	value_extracted=$(eval echo "$envval")
-
-	current_value=$(eval "echo \$$envname")
-
-	if [[ :"$current_value": != *:"$value_extracted":* ]]
-	then
-		echo "export $envname=\"$envval:\$$envname\"" >> "$file"
-	fi
-}
-
-add_line_to_profile() {
-	file="$1"
-	line="$2"
-
-	grep -qF -- "$line" "$file" || echo "$line" >> "$file"
-}
-
-# Locally-installed programs go here.
-add_env_to_profile ~/.bashrc 'PATH' '$HOME/.local/bin'
-add_env_to_profile ~/.bashrc 'LD_LIBRARY_PATH' '$HOME/.local/lib'
-add_env_to_profile ~/.bashrc 'MANPATH' '$HOME/.local/share/man'
-
 # neovim latest version
 mkdir ~/.local/bin -p
 cd ~/.local/bin
@@ -37,13 +11,10 @@ rsync -a squashfs-root/usr/ ~/.local/
 rm nvim.appimage
 rm -rf squashfs-root
 
-add_line_to_profile ~/.bash_aliases 'alias vi=nvim'
-add_line_to_profile ~/.bash_aliases 'alias vim=nvim'
-add_line_to_profile ~/.bash_aliases "alias vimdiff='nvim -d'"
-add_line_to_profile ~/.bashrc "export EDITOR=nvim"
-
 cd -
 \source ~/.bashrc
 
 echo "neovim installed at $HOME/.local/bin/nvim"
-echo "Run 'vi' to start nvim."
+
+echo "Run: source <(curl -sS https://raw.githubusercontent.com/kiyoon/neovim-local-install/master/bash.sh)"
+echo "to add PATH and aliases to ~/.bashrc and ~/.bash_aliases."
